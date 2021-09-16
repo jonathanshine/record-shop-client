1e63fe9dfb7e679caae77448622395ad0f3a128b
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-16 14:40:49 +0200

    added a 'plus' button to each Record in the list and added an onClick method which updates the cart within the User model

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext';
 
 const Record = ({data}) => {
+    const { user, setUser } = useContext(UserContext);
+
+    const addRecordToCart = (id) => {
+        
+        let recordExists = user.cart.find(item => item.record === id);
+
+        if(recordExists) {
+        const updatedCart = user.cart.map((item) => {
+            if(item.record === id) {
+                return {...item, quantity: item.quantity + 1 };
+            } else {
+                return item;
+            };    
+        });
+        setUser({...user, cart: updatedCart});
+        }
+        else {
+            // create temporary cart
+            let newCart = [...user.cart, { record: id, quantity: 1 }];
+    
+            // add the id of the record to the cart inside the user object
+            setUser({ ...user, cart: newCart });
+        }
+    };
+    
+    console.log(user?.cart);
+
     return (
         <div className="record">
             <img src={data.cover} alt="" onError={(event)=>{ return event.target.parentNode.style.display = 'none'}}></img>
             <p>{data.title}</p>
             <small>{data.artist}</small>
+            <div className="add-button" onClick={() => addRecordToCart(data._id)}> + </div>
         </div>
     )
 }
--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -20,7 +20,7 @@ export const SignInUser = async (data) => {
 
 export const SignUpUser = async (data) => {
   try {
-    const res = await(await fetch(`${serverUrl}/users/`, {
+    const res = await(await fetch(`${serverUrl}/users`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
